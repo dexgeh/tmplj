243dc85bbe2923d4185ebfb03caaf05822117084
dexgeh <dexgeh@gmail.com>
2012-02-29 19:44:36 +0100

    escape parameter to Print tag is not required

--- main/tmplj/parser/tagdefs/Print.java
+++ main/tmplj/parser/tagdefs/Print.java
@@ -6,11 +6,12 @@ import tmplj.parser.TagDef;
 
 public class Print extends TagDef {
 	public Print() {
-		super("print", new String[] {"expr","escape"});
+		super("print", new String[] {"expr"});
 	}
 	@Override
 	public void startTag(StringBuilder out, Map<String, String> attributes) {
-		if ("basic-xml".equalsIgnoreCase(attributes.get("escape"))) {
+		String escape = attributes.get("escape");
+		if (escape == null || "basic-xml".equalsIgnoreCase(escape)) {
 			out.append("out.append(tmplj.util.Strings.escapeBasic(\"\" + "+attributes.get("expr")+"));");
 		} else {
 			out.append("out.append("+attributes.get("expr")+");");
